2115b35cd8399514f47821ca120c03af3c5a4007
Martin Jambon <Martin Jambon>
2020-07-20 19:13:57 -0700

    Use ubuntu config

--- config.sh
+++ config.sh
@@ -5,15 +5,15 @@
 
 # The OS family. Determines which collection of install scripts to use.
 # Currently, choices are 'alpine' or 'ubuntu'.
-os="alpine"
+os="ubuntu"
 
 # The argument of the FROM line in the dockerfile. This is the docker
 # URL of the base image, optionally followed by more things.
-from="alpine:3.12.0"
+from="ubuntu"
 
 # This is the argument of 'docker pull', 'docker push', etc. for the image
 # we are building.
-docker_url="mjambon/ocaml:alpine"
+docker_url="mjambon/ocaml:ubuntu"
 
 # User to create and use. If it already exists, we'll try to use it.
 user="user"
