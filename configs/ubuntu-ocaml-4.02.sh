69425de14d9378e526cc0a994cfe7b2f840ca1e1
Martin Jambon <Martin Jambon>
2020-07-25 21:11:13 -0700

    Add configs for ocaml 4.02

---
+++ configs/ubuntu-ocaml-4.02.sh
@@ -0,0 +1,32 @@
+#
+# Configuration for creating a dockerfile, building the docker image, and
+# for pushing it to a docker registry.
+#
+
+# Inherit common settings.
+. ./common-config.sh
+
+# The OS family. Determines which collection of install scripts to use.
+# Currently, choices are 'alpine' or 'ubuntu'.
+os="ubuntu"
+
+# The argument of the FROM line in the dockerfile. This is the docker
+# URL of the base image, optionally followed by more things.
+from="ubuntu"
+
+# This is the argument of 'docker pull', 'docker push', etc. for the image
+# we are building.
+docker_url="mjambon/mj-ocaml-4.03:ubuntu"
+
+# User to create and use. If it already exists, we'll try to use it.
+user="user"
+
+# Extra packages to be installed by the native package manager.
+extra_packages="$extra_deb_packages"
+
+# Opam switch to use. This determines the OCaml version and a set of
+# configuration options.
+opam_switch="4.02.3"
+
+# The collection of opam packages we want to install. Go wild.
+opam_packages="$opam_packages"
