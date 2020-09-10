8432723f478c871bddda13f777b19743fbf1685a
Martin Jambon <Martin Jambon>
2020-09-10 11:53:25 -0700

    Change DockerHub URLs to use returntocorp's account instead of mjambon.

--- configs/alpine.sh
+++ configs/alpine.sh
@@ -16,7 +16,7 @@ from="alpine:3.12.0"
 
 # This is the argument of 'docker pull', 'docker push', etc. for the image
 # we are building.
-docker_url="mjambon/r2c-ocaml:alpine"
+docker_url="returntocorp/ocaml:alpine"
 
 # User to create and use. If it already exists, we'll try to use it.
 user="user"
