6f4bb35e948bc0a927388470557b032d2ffe492c
Martin Jambon <Martin Jambon>
2020-09-30 12:37:13 -0700

    Update list of opam packages to preinstall

--- common-config.sh
+++ common-config.sh
@@ -25,25 +25,29 @@ extra_deb_packages="
 # The collection of opam packages we want to install. Go wild.
 opam_packages="
   alcotest
+  ansiterminal
   atdgen
   cmdliner
   conf-perl
   conf-pkg-config
   dune
   dune-glob
+  easy_logging_yojson
   grain_dypgen
-  json-wheel
   menhir
   num
   ocamlfind
   ocamlgraph
+  ounit2
   parmap
   ppx_deriving
   ppx_sexp_conv
+  re
   sexplib
   tsort
   utop
   uucp
   uutf
   yaml
+  yojson
 "
